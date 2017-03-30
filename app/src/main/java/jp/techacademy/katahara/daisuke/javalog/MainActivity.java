e7d80e7bb6b8f74d630c4d16a1ca2dd1d31a2435
Daisuke <siq009@yahoo.ca>
2017-03-30 16:36:19 -0400

    ログへの出力を加えたバージョン。

--- app/src/main/java/jp/techacademy/katahara/daisuke/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/katahara/daisuke/javalog/MainActivity.java
@@ -2,6 +2,7 @@ package jp.techacademy.katahara.daisuke.javalog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log; //ログ出力 Lesson 3.4
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("javatest", "ログへの出力テスト"); //ログ出力 Lesson 3.4
     }
 }
